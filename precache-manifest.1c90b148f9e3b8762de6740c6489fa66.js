ff18378e038ba6c6929a2f2e7972b54526c03a06
Anastasiia <st.korableva@yandex.ru>
2020-06-06 15:44:27 +0300

    Updates

---
+++ precache-manifest.1c90b148f9e3b8762de6740c6489fa66.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef98de80504af27ce023d0405945505a",
+    "url": "/PizzaApp/index.html"
+  },
+  {
+    "revision": "aa31c438b957ad720cb3",
+    "url": "/PizzaApp/static/css/main.54e5d59a.chunk.css"
+  },
+  {
+    "revision": "0aa6949a6d7d2751d42b",
+    "url": "/PizzaApp/static/js/2.0e7d4ded.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/PizzaApp/static/js/2.0e7d4ded.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa31c438b957ad720cb3",
+    "url": "/PizzaApp/static/js/main.5f228a40.chunk.js"
+  },
+  {
+    "revision": "85e95dc6e4cee7e438b0",
+    "url": "/PizzaApp/static/js/runtime-main.170f9013.js"
+  },
+  {
+    "revision": "ff0665e1a53f6303717df4667745fd4e",
+    "url": "/PizzaApp/static/media/Drinks (1).ff0665e1.jpeg"
+  },
+  {
+    "revision": "a730d68699f484f32719d3dac79d5e4a",
+    "url": "/PizzaApp/static/media/Drinks (2).a730d686.jpeg"
+  },
+  {
+    "revision": "4d6128caa88ea28faf06be4093fc8ad9",
+    "url": "/PizzaApp/static/media/Drinks (3).4d6128ca.jpeg"
+  },
+  {
+    "revision": "ad6896c7bda4b8ac780f91bf0f97573a",
+    "url": "/PizzaApp/static/media/Drinks (4).ad6896c7.jpeg"
+  },
+  {
+    "revision": "c47f44467e9c536dce48393763106924",
+    "url": "/PizzaApp/static/media/Drinks (5).c47f4446.jpeg"
+  },
+  {
+    "revision": "12eb5a65f96c874395384612e71c64af",
+    "url": "/PizzaApp/static/media/Drinks (6).12eb5a65.jpeg"
+  },
+  {
+    "revision": "c6841ee07e82626b49f8d64487815533",
+    "url": "/PizzaApp/static/media/Logo.c6841ee0.jpg"
+  },
+  {
+    "revision": "bfc53b6a6bee1a0b135dddd79dd8468d",
+    "url": "/PizzaApp/static/media/arrow-down.bfc53b6a.svg"
+  },
+  {
+    "revision": "ea1631d7cefa31b73cfe97c88f050696",
+    "url": "/PizzaApp/static/media/arrow-up.ea1631d7.svg"
+  },
+  {
+    "revision": "07be3e2363548775fce0a0f7a3167d46",
+    "url": "/PizzaApp/static/media/close-button.07be3e23.svg"
+  },
+  {
+    "revision": "c6e7ca3df4a61309d775914559827d6b",
+    "url": "/PizzaApp/static/media/empty_cart.c6e7ca3d.jpg"
+  },
+  {
+    "revision": "c0af93c3863f4eef5c7aa69129728111",
+    "url": "/PizzaApp/static/media/pizza (1).c0af93c3.jpeg"
+  },
+  {
+    "revision": "15085ec3f9fb4ecc4403f134d917252b",
+    "url": "/PizzaApp/static/media/pizza (2).15085ec3.jpeg"
+  },
+  {
+    "revision": "f5e47b6a98af1692c608008ff97d962d",
+    "url": "/PizzaApp/static/media/pizza (3).f5e47b6a.jpeg"
+  },
+  {
+    "revision": "d6bde3a652a5ac25bffb320d31a10cfe",
+    "url": "/PizzaApp/static/media/pizza (4).d6bde3a6.jpeg"
+  },
+  {
+    "revision": "89100e89d3ce807ec1efc11a5a3685df",
+    "url": "/PizzaApp/static/media/pizza (5).89100e89.jpeg"
+  },
+  {
+    "revision": "67a833c2de6e46807bdb8814e4711d69",
+    "url": "/PizzaApp/static/media/pizza (6).67a833c2.jpeg"
+  },
+  {
+    "revision": "f5a2bb85c8f1485ff33430b8bf1d2232",
+    "url": "/PizzaApp/static/media/pizza (7).f5a2bb85.jpeg"
+  },
+  {
+    "revision": "381f84bf35d21a5d4d8541cae748165d",
+    "url": "/PizzaApp/static/media/pizza (8).381f84bf.jpeg"
+  },
+  {
+    "revision": "1e71bac71dbb669f76025968bfe92ba4",
+    "url": "/PizzaApp/static/media/plus.1e71bac7.svg"
+  },
+  {
+    "revision": "2f79694de210f873ca01300355f8631f",
+    "url": "/PizzaApp/static/media/shopping-cart.2f79694d.svg"
+  }
+]);
